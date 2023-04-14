46a9efc43288b7403e02389620aba732b67009f1
pavel <pavelsakovic@mail.ru>
2023-04-14 15:37:42 +0300

    Added @Transaction(readOnly = true)

--- src/main/java/org/example/service/UserServiceImpl.java
+++ src/main/java/org/example/service/UserServiceImpl.java
@@ -33,12 +33,14 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getAllUsers() {
         log.info("List  was got users");
         return userRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getUserById(int id) throws UserOutputException {
         return userRepository.findById(id).orElseThrow(() -> new UserOutputException("User with index " + id + "does not exist"));
     }
